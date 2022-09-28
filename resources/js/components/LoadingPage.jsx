b114fd775320c707da56cf5dd4fd1fa9064b8073
Mohamad Adeeb Allam (Official Git) <mohamadadeeballam@hotmail.com>
2022-09-28 10:14:56 +0300

    remove some unsed comments

--- resources/js/components/LoadingPage.jsx
+++ resources/js/components/LoadingPage.jsx
@@ -3,14 +3,6 @@ import useMediaQuery from "@mui/material/useMediaQuery";
 import CircularProgress from "@mui/material/CircularProgress";
 import { makeStyles } from "@mui/styles";
 const useStyles = makeStyles({
-    //     mobile stepper: 1000
-    // fab: 1050
-    // speed dial: 1050
-    // app bar: 1100
-    // drawer: 1200
-    // modal: 1300
-    // snackbar: 1400
-    // tooltip: 1500
     root: {
         display: "flex",
         justifyContent: "center",
